c723377e6255f696c4b0ecfb6aed88c8de08ae63
Amavidato <amatodavide1@gmail.com>
2024-09-09 20:30:36 +0200

    Implemented movement constraint to force the player to stay inside the the borders.

--- Core/src/my_ecs_stuff/MovementSystem.cpp
+++ Core/src/my_ecs_stuff/MovementSystem.cpp
@@ -2,6 +2,9 @@
 
 #include "Components.h"
 #include "../ecs_core/EcsManager.h"
+#include "../settings/PlayerSettings.h"
+#include "../settings/GameSettings.h"
+
 void MovementSystem::OnCreate()
 {
 	signature = EcsManager::CreateSignature<Position2D, Speed, Direction2D, PlayerTag>();
@@ -14,7 +17,19 @@ void MovementSystem::OnUpdate(float deltaTime, EcsManager* ecsManager)
 		auto& position = ecsManager->GetComponent<Position2D>(entity);
 		auto& speed = ecsManager->GetComponent<Speed>(entity);
 		auto& direction = ecsManager->GetComponent<Direction2D>(entity);
-		position.x += static_cast<float>(speed.value) * deltaTime * direction.x;
+		auto movementX = static_cast<float>(speed.value) * deltaTime * direction.x;
+		if (IsBlockedByBorder(position.x, movementX))
+			return;
+		position.x += movementX;
 		position.y += static_cast<float>(speed.value) * deltaTime * direction.y;
 	}
+}
+
+bool MovementSystem::IsBlockedByBorder(const float positionX, const float movementX) const 
+{
+	auto resultingPositionX = positionX + movementX;
+	if (resultingPositionX < 0 || resultingPositionX + PlayerSettings::playerWidth > GameSettings::windowHeight)
+		return true;
+	return false;
+
 }
--- Core/src/settings/PlayerSettings.h
+++ Core/src/settings/PlayerSettings.h
@@ -6,15 +6,15 @@ class PlayerSettings
 {
 public:
 	static constexpr const char* texturePath {"assets/player_white.png"};
-	static constexpr int speed = 100;
-	static constexpr int playerWidth = 48;
-	static constexpr int playerHeight = 48;
+	static constexpr int speed {100};
+	static constexpr int playerWidth {48};
+	static constexpr int playerHeight {48};
 	struct BulletSettings
 	{
-		static constexpr const char* bulletTexturePath {"assets/player_white.png"};
-		static constexpr int bulletWidth = 3;
-		static constexpr int bulletHeight = 20;
-		static constexpr int speed = 300;
+		static constexpr const char* texturePath {"assets/player_white.png"};
+		static constexpr int width {3};
+		static constexpr int height {20};
+		static constexpr int speed {300};
 	};
 };
 
